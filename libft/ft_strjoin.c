958787dfb62a4ed8977b9c5845b5215b6bc4edb5
Kensei Nakamura <kenakamu@c4r1s5.42tokyo.jp>
2024-11-27 23:17:50 +0900

    substr

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kenakamu <kenakamu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/31 21:25:32 by kenakamu          #+#    #+#             */
-/*   Updated: 2024/11/27 18:16:31 by kenakamu         ###   ########.fr       */
+/*   Created: 2024/11/27 22:42:14 by kenakamu          #+#    #+#             */
+/*   Updated: 2024/11/27 22:43:01 by kenakamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t		n;
-	size_t	src_len;
-
-	i = 0;
-	n = size;
-	src_len = ft_strlen(src);
-	if (size == 0)
-		return (src_len);
-	while (n != 0 && --n != 0)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (src_len);
+	
 }
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: kenakamu <kenakamu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 18:33:31 by kenakamu          #+#    #+#             */
-/*   Updated: 2024/11/26 21:50:40 by kenakamu         ###   ########.fr       */
+/*   Updated: 2024/11/27 22:47:21 by kenakamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,43 @@
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
-	unsigned int	i;
 
 	if (!s)
 		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
 	if (len > ft_strlen(s + start))
 		len = ft_strlen(s + start);
 	str = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s, len + 1);
+	ft_strlcpy(str, s + start, len + 1);
 	return (str);
 }
+//int main(void)
+//{
+//    // テストケース1: 通常の部分文字列
+//    const char *str1 = "Hello, World!";
+//    char *result1 = ft_substr(str1, 7, 5);  // "World"を取得
+//    printf("Test1: '%s'\n", result1);
+//    free(result1);
+
+//    // テストケース2: 文字列の最初から
+//    const char *str2 = "42Tokyo";
+//    char *result2 = ft_substr(str2, 0, 2);  // "42"を取得
+//    printf("Test2: '%s'\n", result2);
+//    free(result2);
+
+//    // テストケース3: 長さが元の文字列より長い
+//    const char *str3 = "Test";
+//    char *result3 = ft_substr(str3, 1, 10);  // "est"を取得
+//    printf("Test3: '%s'\n", result3);
+//    free(result3);
+
+//    // テストケース4: NULLポインタ
+//    char *result4 = ft_substr(NULL, 0, 5);
+//    printf("Test4: '%s'\n", result4 ? result4 : "NULL");
+//    free(result4);
+
+//    return (0);
+//}
